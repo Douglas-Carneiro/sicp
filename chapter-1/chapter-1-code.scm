81c3c94e45cc592ae695848cfe7a121078b1df58
Douglas-Carneiro <douglasdscplay@gmail.com>
2024-08-18 18:06:51 -0300

    exercise 1.16

--- chapter-1/chapter-1-code.scm
+++ chapter-1/chapter-1-code.scm
@@ -488,3 +488,44 @@
 
 ;; b. (sine a) generates a linear recursive procedure,
 ;; so the space and number of steps grow linearly with proportion to a -> O(a)
+
+;; 1.2.4 Exponentiation
+
+(define (expt b n)
+  (if (= n 0)
+      1
+      (* b (expt b (- n 1)))))
+
+;; iterative version:
+(define (expt b n)
+  (expt-iter b n 1))
+(define (expt-iter b counter product)
+  (if (= counter 0)
+      product
+      (expt-iter b
+		 (- counter 1)
+		 (* b product))))
+
+(define (fast-expt b n)
+  (cond ((= n 0) 1)
+	((even? n) (square (fast-expt b (/ n 2))))
+	(else (* b (fast-expt b (- n 1))))))
+;; Exercise 1.16
+;; First try (wrong solution):
+;(define (fast-expt b n)
+ ; (fast-expt-iter b n 1))
+;(define (fast-expt-iter b counter product)
+ ; (cond ((= counter 0) product)
+;	((= counter 2) (if (= product 1)
+;			   (fast-expt-iter b 0  (square b))
+;			   (fast-expt-iter b 0 (square product))))
+;	((even? counter) (fast-expt-iter b (/ counter 2) (* product (square b))))
+;	(else (fast-expt-iter b (- counter 1) (* product b)))))
+
+; Correct solution (source: https://sicp-solutions.net/post/sicp-solution-exercise-1-16/)
+(define (fast-expt b n)
+  (fast-expt-iter 1 b n))
+(define (fast-expt-iter a b n)
+  (cond ((= n 0) a)
+	((even? n) (fast-expt-iter a (square b) (/ n 2)))
+	(else (fast-expt-iter (* a b) b (- n 1)))))
