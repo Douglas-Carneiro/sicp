661b5e9d6779ca0a84ef04f1c1fa34bc3a73f91d
Douglas-Carneiro <douglasdscplay@gmail.com>
2024-08-31 19:54:05 -0300

    exercise 1.20

--- chapter-1/chapter-1-code.scm
+++ chapter-1/chapter-1-code.scm
@@ -566,3 +566,80 @@
 			p
 			q
 			(- count 1)))))
+
+;; 1.2.5 Greatest Common Divisors
+
+(define (gcd a b)
+  (if (= b 0)
+      a
+      (gcd b (remainder a b))))
+
+;; Exercise 1.20
+;; Illustration of process for (gcd 206 40) using normal-order evaluation:
+;; (gcd 206 40)
+;; (if (= 40 0) 206 (gcd 40 (remainder 206 40)))
+
+;; (gcd 40 (remainder 206 40))
+;; (if (= (remainder 206 40) 0) 40 (gcd (remainder 206 40) (remainder 40 (remainder 206 40))))
+
+;; (gcd (remainder 206 40) (remainder 40 (remainder 206 40)))
+;; (if (= (remainder 40 (remainder 206 40)) 0)
+;;    (remainder 206 40)
+;;    (gcd (remainder 40 (remainder 206 40))
+;;	   (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))))
+
+;; (gcd (remainder 40 (remainder 206 40))
+;;	   (remainder (remainder 206 40) (remainder 40 (remainder 206 40))))
+;; (if (= (remainder (remainder 206 40) (remainder 40 (remainder 206 40))) 0)
+;;     (remainder 40 (remainder 206 40))
+;;     (gcd
+;;      (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))
+;;      (remainder (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40))))))
+
+;; (gcd
+;;   (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))
+;;   (remainder (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))))
+;; (if (= (remainder (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))) 0)
+;;     (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))
+;;     (gcd
+;;      (remainder (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40))))
+;;      (remainder (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))
+;;		(remainder (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))))))
+
+;; Q: How many remainder operations are actually performed in the normal-order evaluation of (gcd 206 40)?
+;; A: the number of remainder operations performed in the normal-order evaluation of (gcd 206 40) is 18,
+;; 14 times in the if's to check the logical value of the expression and 4 times for the consequent expression of the if.
+
+;; Illustration of process for (gcd 206 40) using applicative-order evaluation:
+;; (gcd 206 40)
+;; (if (= 40 0)
+;;     206
+;;     (gcd 40 (remainder 206 40)))
+
+;; (gcd 40 (remainder 206 40)) --> evaluates the arguments
+;; (gcd 40 6)
+;; (if (= 6 0)
+;;     40
+;;     (gcd 6 (remainder 40 6)))
+
+;; (gcd 6 (remainder 40 6)) --> evaluates the arguments
+;; (gcd 6 4)
+;; (if (= 4 0)
+;;     6
+;;     (gcd 4 (remainder 6 4)))
+
+;; (gcd 4 (remainder 6 4)) --> evaluates the arguments
+;; (gcd 4 2)
+;; (if (= 2 0)
+;;     4
+;;     (gcd 2 (remainder 4 2)))
+
+;; (gcd 2 (remainder 4 2)) --> evaluates the arguments
+;; (gcd 2 0)
+;; (if (= 0 0)
+;;     2
+;;     (gcd 0 (remainder 2 0)))
+;; 2
+
+;; Q: In the applicative-order evaluation?
+;; A: the number of remainder operations performed in the applicative-order evaluation of (gcd 206 40) is 4
