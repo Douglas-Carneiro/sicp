4273c26a7e5bf7d8d36e17a8e0ffd535aa02fb1d
Douglas-Carneiro <douglasdscplay@gmail.com>
2024-11-21 16:24:08 -0300

    exercise 1.29

--- chapter-1/chapter-1-code.scm
+++ chapter-1/chapter-1-code.scm
@@ -942,3 +942,69 @@
 	(else false)))
 
 ;; TODO: Study mutual recursion to really understand the solution
+
+;; 1.3 Formulating Abstractions with Higher-Order Procedures
+
+;; 1.3.1 Procedures as Arguments
+ (define (sum-integers a b)
+   (if (> a b)
+       0
+       (+ a (sum-integers (+ a 1) b))))
+
+(define (sum-cubes a b)
+  (if (> a b)
+      0
+      (+ (cube a)
+	 (sum-cubes (+ a 1) b))))
+
+(define (pi-sum a b)
+  (if (> a b)
+      0
+      (+ (/ 1.0 (* a (+ a 2)))
+	 (pi-sum (+ a 4) b))))
+
+(define (sum term a next b)
+  (if (> a b)
+      0
+      (+ (term a)
+	 (sum term (next a) next b))))
+
+(define (inc n) (+ n 1))
+(define (sum-cubes a b)
+  (sum cube a inc b))
+
+(define (identity x) x)
+(define (sum-integers a b)
+  (sum identity a inc b))
+
+(define (pi-sum a b)
+  (define (pi-term x)
+    (/ 1.0 (* x (+ x 2))))
+  (define (pi-next x)
+    (+ x 4))
+  (sum pi-term a pi-next b))
+
+(* 8 (pi-sum 1 1000))
+
+(define (integral f a b dx)
+  (define (add-dx x)
+    (+ x dx))
+  (* (sum f (+ a (/ dx 2.0)) add-dx b)
+     dx))
+
+(integral cube 0 1 0.01) ;; => .24998750000000042
+(integral cube 0 1 0.001) ;; => .249999875000001
+
+;; Exercise 1.29
+
+(define (simpson-integral f a b n)
+  (define h (/ (- b a) n))
+  (define (y n) (f (+ a (* n h))))
+  (define (integral-term x)
+    (if (even? x)
+	(* 2 (y x))
+	(* 4 (y x))))
+  (* (/ h 3) (+ (y 0) (y n) (sum integral-term 1 inc (- n 1)))))
+
+(simpson-integral cube 0 1 100) ;; => 1/4
+(simpson-integral cube 0 1 1000) ;; => 1/4
