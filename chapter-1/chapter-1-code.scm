70e0285dbbc9955d6358967fbc7271206dc2c0e6
Douglas-Carneiro <douglasdscplay@gmail.com>
2024-11-23 19:06:36 -0300

    exercise 1.32

--- chapter-1/chapter-1-code.scm
+++ chapter-1/chapter-1-code.scm
@@ -1047,3 +1047,26 @@
 	result
 	(iter (next a) (* (term a) result))))
   (iter a 1))
+
+;; Exercise 1.32
+;; a.
+(define (accumulate combiner null-value term a next b)
+  (if (> a b)
+      null-value
+      (combiner (term a)
+		(accumulate combiner null-value term (next a) next b))))
+
+;; Defining sum and product in terms of accumulate
+(define (sum term a next b)
+  (accumulate + 0 term a next b))
+
+(define (product term a next b)
+  (accumulate * 1 term a next b))
+
+;; b. Iterative version of accumulate
+(define (accumulate combiner null-value term a next b)
+  (define (iter a result)
+    (if (> a b)
+	result
+	(iter (next a) (combiner (term a) result))))
+  (iter a null-value))
